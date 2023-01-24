55ab9e38bd9cb8f98eb7565d8952ca0c0b097c61
LillyNjoki <lillyannenjoki@gmail.com>
2023-01-24 15:45:03 +0000

    completed Assignment

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import blogData from "../data/blog";
+import Header from "../components/Header";
+import About from "../components/About";
+import ArticleList from "../components/ArticleList";
+
+
+console.log(blogData);
+
+function App() {
+  return (
+    <div className="App">
+      <Header name={blogData.name}/>
+      <About image={blogData.image} text={blogData.about}/>
+      <ArticleList/>
+
+
+    </div>
+  );
+}
+
+export default App;
